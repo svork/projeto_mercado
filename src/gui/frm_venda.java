70862e2b7ff84348660793a24f2bc774e14ec18e
Rodrigo Costa <svork@users.noreply.github.com>
2018-05-21 22:11:37 -0300

    Função Incluir Produto

--- src/gui/frm_venda.java
+++ src/gui/frm_venda.java
@@ -10,9 +10,6 @@ public class frm_venda extends javax.swing.JFrame {
     // Instancia da classe de Conexão
     Banco banco;
 
-    // controlar erro de navegacao nos botoes proximo e anterior
-    int navega = 0;
-
     // Construtor
     public frm_venda() {
         initComponents();
@@ -26,20 +23,6 @@ public class frm_venda extends javax.swing.JFrame {
         // Campos que não devem ser alterados, somente leitura
         txt_descricao_produto.setEditable(false);
         txt_valor_unitario_produto.setEditable(false);
-        
-    }
-    
-    // Método logar - faz o login de um funcionario
-    public void logar() {
-        try {
-        String login = txt_login.getText();
-        
-        String sql = "select * from funcionario where id_fun = "+ login;
-        
-        lbl_nome_cliente.setText(banco.resultset.getString(1)); // Nome
-        } catch (SQLException e) {
-            JOptionPane.showMessageDialog(null, "Erro ao acessar dados\n" + e, "Erro!", JOptionPane.ERROR_MESSAGE);
-        }
         
     }
 
@@ -77,7 +60,7 @@ public class frm_venda extends javax.swing.JFrame {
         
     }
     
-    // Método pontos_desconto - Mostra a quantidade de pontos do cliente
+    // Método buscar_cliente - Mostra a quantidade de pontos do cliente
     public void buscar_cliente(int codigo){
         try{
             //Comando SQL
@@ -102,8 +85,21 @@ public class frm_venda extends javax.swing.JFrame {
             JOptionPane.showMessageDialog(null, "Erro! Cliente não encontrado\n" + e, "Erro", JOptionPane.ERROR_MESSAGE);
         }
     }
-            
-            
+    
+    // Método usar_pontos_desconto - Usa os pontos do cliente para dar desconto no valor total
+    public void usar_pontos_desconto(){
+        
+    }
+    
+    // Método incluir_produto_tabela - Insere o produto que foi buscado na tabela de produtos
+    public void incluir_produto_tabela(){
+        
+    }
+    
+    // Método calcular_valor_total - Mostra o valor total da venda a cada produto que foi inserido
+    public void calcular_valor_total(){
+        
+    }
             
             
     @SuppressWarnings("unchecked")
@@ -111,7 +107,6 @@ public class frm_venda extends javax.swing.JFrame {
     private void initComponents() {
 
         lbl_total_palavra = new javax.swing.JLabel();
-        btn_logar = new javax.swing.JButton();
         txt_observacoes_venda = new javax.swing.JTextField();
         txt_descricao_produto = new javax.swing.JTextField();
         lbl_titulo = new javax.swing.JLabel();
@@ -122,7 +117,6 @@ public class frm_venda extends javax.swing.JFrame {
         lbl_descricao_produto = new javax.swing.JLabel();
         lbl_numero_venda = new javax.swing.JLabel();
         lbl_data_venda = new javax.swing.JLabel();
-        txt_login = new javax.swing.JTextField();
         lbl_hora_venda = new javax.swing.JLabel();
         lbl_funcionario = new javax.swing.JLabel();
         btn_voltar = new javax.swing.JButton();
@@ -134,7 +128,6 @@ public class frm_venda extends javax.swing.JFrame {
         txt_codigo_produto = new javax.swing.JTextField();
         btn_confirmar = new javax.swing.JButton();
         lbl_rodape_venda = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
         lbl_pontos_cliente_cabecalho = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
@@ -146,6 +139,7 @@ public class frm_venda extends javax.swing.JFrame {
         lbl_valor_desconto_cliente = new javax.swing.JLabel();
         painel_tabela = new javax.swing.JScrollPane();
         tabela_lista_produto = new javax.swing.JTable();
+        lbl_codigo_cliente = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setResizable(false);
@@ -155,18 +149,6 @@ public class frm_venda extends javax.swing.JFrame {
         lbl_total_palavra.setForeground(new java.awt.Color(255, 255, 255));
         lbl_total_palavra.setText("Total: R$");
         getContentPane().add(lbl_total_palavra, new org.netbeans.lib.awtextra.AbsoluteConstraints(470, 600, 130, 40));
-
-        btn_logar.setBackground(new java.awt.Color(204, 204, 204));
-        btn_logar.setForeground(new java.awt.Color(0, 51, 51));
-        btn_logar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/Template/botao0.png"))); // NOI18N
-        btn_logar.setText("Logar");
-        btn_logar.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
-        btn_logar.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btn_logarActionPerformed(evt);
-            }
-        });
-        getContentPane().add(btn_logar, new org.netbeans.lib.awtextra.AbsoluteConstraints(710, 40, 100, 20));
 
         txt_observacoes_venda.setForeground(new java.awt.Color(204, 204, 204));
         txt_observacoes_venda.setText("Observações");
@@ -215,21 +197,20 @@ public class frm_venda extends javax.swing.JFrame {
         lbl_numero_venda.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         lbl_numero_venda.setForeground(new java.awt.Color(0, 51, 102));
         lbl_numero_venda.setText("Pedido N° 1626");
-        getContentPane().add(lbl_numero_venda, new org.netbeans.lib.awtextra.AbsoluteConstraints(850, 150, -1, -1));
+        getContentPane().add(lbl_numero_venda, new org.netbeans.lib.awtextra.AbsoluteConstraints(850, 80, -1, -1));
 
         lbl_data_venda.setBackground(new java.awt.Color(204, 204, 204));
         lbl_data_venda.setForeground(new java.awt.Color(153, 153, 153));
         lbl_data_venda.setText("20/04/2018");
-        getContentPane().add(lbl_data_venda, new org.netbeans.lib.awtextra.AbsoluteConstraints(850, 180, -1, -1));
-        getContentPane().add(txt_login, new org.netbeans.lib.awtextra.AbsoluteConstraints(730, 70, 60, -1));
+        getContentPane().add(lbl_data_venda, new org.netbeans.lib.awtextra.AbsoluteConstraints(850, 110, -1, -1));
 
         lbl_hora_venda.setForeground(new java.awt.Color(153, 153, 153));
         lbl_hora_venda.setText("22:59");
-        getContentPane().add(lbl_hora_venda, new org.netbeans.lib.awtextra.AbsoluteConstraints(940, 180, -1, -1));
+        getContentPane().add(lbl_hora_venda, new org.netbeans.lib.awtextra.AbsoluteConstraints(940, 110, -1, -1));
 
         lbl_funcionario.setForeground(new java.awt.Color(153, 153, 153));
         lbl_funcionario.setText("Nome Usuário");
-        getContentPane().add(lbl_funcionario, new org.netbeans.lib.awtextra.AbsoluteConstraints(880, 210, -1, -1));
+        getContentPane().add(lbl_funcionario, new org.netbeans.lib.awtextra.AbsoluteConstraints(880, 140, -1, -1));
 
         btn_voltar.setBackground(new java.awt.Color(0, 204, 0));
         btn_voltar.setFont(new java.awt.Font("Arial Rounded MT Bold", 0, 14)); // NOI18N
@@ -263,7 +244,7 @@ public class frm_venda extends javax.swing.JFrame {
                 btn_pesquisar_codigo_produtoActionPerformed(evt);
             }
         });
-        getContentPane().add(btn_pesquisar_codigo_produto, new org.netbeans.lib.awtextra.AbsoluteConstraints(840, 50, 80, 60));
+        getContentPane().add(btn_pesquisar_codigo_produto, new org.netbeans.lib.awtextra.AbsoluteConstraints(560, 90, 100, 60));
 
         txt_valor_unitario_produto.setFocusable(false);
         getContentPane().add(txt_valor_unitario_produto, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 110, 120, -1));
@@ -281,9 +262,6 @@ public class frm_venda extends javax.swing.JFrame {
         lbl_rodape_venda.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/Template/bar4.png"))); // NOI18N
         getContentPane().add(lbl_rodape_venda, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 600, -1, -1));
 
-        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/Template/bar2.png"))); // NOI18N
-        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 40, -1, -1));
-
         jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/pagamento.png"))); // NOI18N
         getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
 
@@ -297,10 +275,10 @@ public class frm_venda extends javax.swing.JFrame {
         btn_incluir_produto.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/Template/botao1.png"))); // NOI18N
         btn_incluir_produto.setText("Incluir");
         btn_incluir_produto.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
-        getContentPane().add(btn_incluir_produto, new org.netbeans.lib.awtextra.AbsoluteConstraints(930, 50, 70, 60));
+        getContentPane().add(btn_incluir_produto, new org.netbeans.lib.awtextra.AbsoluteConstraints(670, 90, 100, 60));
 
         jLabel14.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/Template/bar5.png"))); // NOI18N
-        getContentPane().add(jLabel14, new org.netbeans.lib.awtextra.AbsoluteConstraints(830, 120, 170, 120));
+        getContentPane().add(jLabel14, new org.netbeans.lib.awtextra.AbsoluteConstraints(830, 50, 170, 120));
 
         btn_pontos_descontos.setText("Usar Pontos");
         getContentPane().add(btn_pontos_descontos, new org.netbeans.lib.awtextra.AbsoluteConstraints(850, 440, -1, -1));
@@ -314,7 +292,7 @@ public class frm_venda extends javax.swing.JFrame {
         getContentPane().add(btn_buscar_codigo_cliente, new org.netbeans.lib.awtextra.AbsoluteConstraints(860, 270, -1, -1));
 
         txt_codigo_cliente.setHorizontalAlignment(javax.swing.JTextField.CENTER);
-        getContentPane().add(txt_codigo_cliente, new org.netbeans.lib.awtextra.AbsoluteConstraints(860, 240, 100, -1));
+        getContentPane().add(txt_codigo_cliente, new org.netbeans.lib.awtextra.AbsoluteConstraints(860, 240, 110, -1));
 
         lbl_valor_desconto_cliente.setText("Desconto R$: ");
         getContentPane().add(lbl_valor_desconto_cliente, new org.netbeans.lib.awtextra.AbsoluteConstraints(850, 410, -1, -1));
@@ -438,6 +416,9 @@ public class frm_venda extends javax.swing.JFrame {
 
         getContentPane().add(painel_tabela, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 150, 770, 440));
 
+        lbl_codigo_cliente.setText("Código do Cliente");
+        getContentPane().add(lbl_codigo_cliente, new org.netbeans.lib.awtextra.AbsoluteConstraints(860, 220, -1, -1));
+
         pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
@@ -451,10 +432,6 @@ public class frm_venda extends javax.swing.JFrame {
         this.dispose();
         voltar_menu();
     }//GEN-LAST:event_btn_voltarActionPerformed
-
-    private void btn_logarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_logarActionPerformed
-        logar();
-    }//GEN-LAST:event_btn_logarActionPerformed
 
     private void btn_pesquisar_codigo_produtoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_pesquisar_codigo_produtoActionPerformed
         // Este método aceita apenas numeros inteiros como código do produtp
@@ -522,14 +499,13 @@ public class frm_venda extends javax.swing.JFrame {
     private javax.swing.JButton btn_buscar_codigo_cliente;
     private javax.swing.JButton btn_confirmar;
     private javax.swing.JButton btn_incluir_produto;
-    private javax.swing.JButton btn_logar;
     private javax.swing.JButton btn_pesquisar_codigo_produto;
     private javax.swing.JButton btn_pontos_descontos;
     private javax.swing.JButton btn_voltar;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel14;
-    private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel lbl_codigo_cliente;
     private javax.swing.JLabel lbl_codigo_produto;
     private javax.swing.JLabel lbl_data_venda;
     private javax.swing.JLabel lbl_descricao_produto;
@@ -551,7 +527,6 @@ public class frm_venda extends javax.swing.JFrame {
     private javax.swing.JTextField txt_codigo_cliente;
     private javax.swing.JTextField txt_codigo_produto;
     private javax.swing.JTextField txt_descricao_produto;
-    private javax.swing.JTextField txt_login;
     private javax.swing.JTextField txt_observacoes_venda;
     private javax.swing.JTextField txt_quantidade_produto;
     private javax.swing.JTextField txt_valor_unitario_produto;
